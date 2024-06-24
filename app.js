8d51ea7e07e7f99d66be0cf06f85bd439f33a24e
Benedict0530 <mendozabenedict14@gmail.com>
2024-06-24 11:20:04 +0800

    Back End API

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; // Port to run the server (default: 3000)
+
+// Define a route that returns the JSON response
+app.get('/api/record', (req, res) => {
+    const responseData = {
+        "record": {
+            "dbindex": 1,
+            "country": "BR",
+            "gamelink": "https://7xx93.com/#/main/inicio?ch=9938&sd=6",
+            "status": 1,
+            "policylink": "https://privacy-4e80f.web.app/",
+            "dbversion": 3
+        }
+    };
+    res.json(responseData);
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
